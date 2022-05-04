d0be943f71e715f6aaee7f3124115e34869c8bd6
George Fischer <me@george.id.au>
2022-05-04 16:41:45 +1000

    slightly change step wording

--- steps.ts
+++ steps.ts
@@ -1,9 +1,9 @@
 const steps = new Map([
   ["/", "Introduction"],
-  ["/steps/1-setup", "Setting Up"],
+  ["/steps/1-setup", "Setting up"],
   ["/steps/2-console", "Getting a steam console"],
   ["/steps/3-manifest", "Building a download command"],
-  ["/steps/4-finish-line", "Finishing Up"],
+  ["/steps/4-finish-line", "Finishing steps"],
 ]);
 
 export default steps;
